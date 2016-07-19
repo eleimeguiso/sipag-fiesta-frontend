9208c98065b0e327ec0209312cb72925fe4d3eda
eleimeguiso <eleiimeguiso@gmail.com>
2016-07-19 18:18:30 +0800

    Edited technology view and controller (side nav not final)

--- public/controllers/view-technology.controller.js
+++ public/controllers/view-technology.controller.js
@@ -12,6 +12,7 @@
                 accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
               });
               $('.materialboxed').materialbox();
+              $(".button-collapse").sideNav();
             });
 
             CommodityService.getCommodities()
